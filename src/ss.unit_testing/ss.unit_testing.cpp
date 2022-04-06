c268f6a7f418dff71d81799e88b6da5913253ef9
MCBerberov19 <MCBerberov19@codingburgas.bg>
2022-04-06 20:11:58 +0300

    Add unit testing project

---
+++ src/ss.unit_testing/ss.unit_testing.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ss::unit_testing
+{
+	TEST_CLASS(ssunittesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
