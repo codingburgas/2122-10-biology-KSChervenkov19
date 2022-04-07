e6508ada4c48962b0dccd1b6c9403000403d4f19
MCBerberov19 <MCBerberov19@codingburgas.bg>
2022-04-07 18:08:11 +0300

    Implement json converter functions for Cycles

--- src/ss.types/src/statistics.cpp
+++ src/ss.types/src/statistics.cpp
@@ -13,3 +13,14 @@ void ss::types::from_json(const json &j, ss::types::Trait &trait)
     j.at("Sense").get_to(trait.sense);
     j.at("Speed").get_to(trait.speed);
 }
+
+void ss::types::to_json(json& j, const ss::types::Cycle& cycle)
+{
+    j = json{ {"Alive", cycle.lastedEntities}, {"Traits", json(cycle.traitsInfo)} };
+}
+
+void ss::types::from_json(const json& j, ss::types::Cycle& cycle)
+{
+    j.at("Alive").get_to(cycle.lastedEntities);
+    j.at("Traits").get_to(cycle.traitsInfo);
+}
