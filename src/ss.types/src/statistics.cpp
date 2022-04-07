3136e1356d46467bf22ed7e0df2921f6ba577b72
MCBerberov19 <MCBerberov19@codingburgas.bg>
2022-04-07 18:06:55 +0300

    Implement json converter functions for traits

--- src/ss.types/src/statistics.cpp
+++ src/ss.types/src/statistics.cpp
@@ -2,3 +2,14 @@
 #include "pch_types.h"
 #include "statistics.h"
 // clang-format on
+
+void ss::types::to_json(json& j, const ss::types::Trait& trait)
+{
+    j = json{ {"Sense", trait.sense}, {"Speed", trait.speed} };
+}
+
+void ss::types::from_json(const json& j, ss::types::Trait& trait)
+{
+    j.at("Sense").get_to(trait.sense);
+    j.at("Speed").get_to(trait.speed);
+}
