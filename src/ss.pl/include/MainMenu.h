ee68d63662dfbcdd6d0326bb8651f3f0bbc3ef44
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2022-04-10 22:44:48 +0300

    Add graphs menu with easeInOutExpo animation

--- src/ss.pl/include/MainMenu.h
+++ src/ss.pl/include/MainMenu.h
@@ -5,12 +5,21 @@
 
 class MainMenu : public Scene
 {
-  private:
-    Texture2D buttonTexture;
-    Texture2D logoTexture;
+private:
+    Texture2D simulatorButton_Texture;
+    Texture2D logo_Texture;
+    Texture2D graphsContainer_Texture;
+    Texture2D graphsMenu_Texture;
+    bool graphsIsAnimatingIn = false;
+    bool graphsIsAnimatingOut = false;
+    bool graphsIsOut = false;
+    float graphsContainerPos = -887;
+    float drag = 3000;
 
-  public:
-    MainMenu(std::string sceneName, SceneManager &sceneManager);
+    float animateGraphsContainer();
+
+public:
+    MainMenu(std::string sceneName, SceneManager& sceneManager);
 
     void Start();
 
@@ -18,7 +27,7 @@ class MainMenu : public Scene
 
     void onExit();
 
-    SceneManager &m_sceneManager;
+    SceneManager& m_sceneManager;
 
     Font font;
 };
--- src/ss.pl/src/MainMenu.cpp
+++ src/ss.pl/src/MainMenu.cpp
@@ -1,33 +1,85 @@
 #include "MainMenu.h"
 
-MainMenu::MainMenu(std::string sceneName, SceneManager &sceneManager) : Scene(sceneName), m_sceneManager(sceneManager)
+MainMenu::MainMenu(std::string sceneName, SceneManager& sceneManager) : Scene(sceneName), m_sceneManager(sceneManager)
 {
 }
 
 void MainMenu::Start() // called once, at the start of the scene
 {
     font = LoadFontEx("../../assets/OpenSans.ttf", 96, 0, 0);
-    buttonTexture = LoadTexture("../../assets/button.png");
-    logoTexture = LoadTexture("../../assets/Logo_Transparent_Light.png");
+    simulatorButton_Texture = LoadTexture("../../assets/assets_MainMenu/Simulator_Button.png");
+    logo_Texture = LoadTexture("../../assets/Logo_Transparent_Light.png");
+    graphsContainer_Texture = LoadTexture("../../assets/assets_MainMenu/Graphs_Container.png");
+    graphsMenu_Texture = LoadTexture("../../assets/assets_MainMenu/Graphs_Button.png");
 }
 
 void MainMenu::Update() // called every frame
 {
+    if (graphsIsAnimatingIn)
+    {
+        graphsContainerPos += animateGraphsContainer();
+    }
+    if (graphsIsAnimatingOut)
+    {
+        graphsContainerPos -= animateGraphsContainer();
+    }
+    if (graphsContainerPos >= 0)
+    {
+        graphsIsAnimatingIn = false;
+        drag = 3000;
+        graphsContainerPos = 0;
+        graphsIsOut = true;
+    }
+    if (graphsContainerPos <= -887)
+    {
+        graphsIsAnimatingOut = false;
+        drag = 3000;
+        graphsContainerPos = -887;
+    }
+
     BeginDrawing();
     ClearBackground({ 4, 12, 32, 255 });
     //ClearBackground(WHITE);
 
-    DrawTexture(buttonTexture, 525, 736, WHITE);
-    DrawTexture(logoTexture, 310, 219, WHITE);
+    DrawTexture(simulatorButton_Texture, 525, 736, WHITE);
+    DrawTexture(logo_Texture, 310, 219, WHITE);
+    DrawTexture(graphsContainer_Texture, graphsContainerPos, 0, WHITE);
+    DrawTexture(graphsMenu_Texture, 57, 53, WHITE);
     EndDrawing();
 
     Vector2 mousePos = GetMousePosition();
 
-    if (CheckCollisionPointRec(mousePos, { 525, 736, static_cast<float>(buttonTexture.width), static_cast<float>(buttonTexture.height) }))
+    if (CheckCollisionPointRec(mousePos, { 525, 736, static_cast<float>(simulatorButton_Texture.width), static_cast<float>(simulatorButton_Texture.height) }))
     {
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
             m_sceneManager.setCurrentScene("Simulation");
+        }
+        SetMouseCursor(4);
+    }
+    else
+    {
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+    }
+
+    if (CheckCollisionPointRec(mousePos, { 57, 53, static_cast<float>(graphsMenu_Texture.width), static_cast<float>(graphsMenu_Texture.height) }) && !graphsIsAnimatingIn && !graphsIsAnimatingOut)
+    {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            graphsIsAnimatingIn = true;
+        }
+        SetMouseCursor(4);
+    }
+    else
+    {
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+    }
+
+    if (!CheckCollisionPointRec(mousePos, { 0, 0, static_cast<float>(graphsContainer_Texture.width), static_cast<float>(graphsContainer_Texture.height) }) && graphsIsOut && !graphsIsAnimatingIn && !graphsIsAnimatingOut)
+    {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            graphsIsAnimatingOut = true;
         }
         SetMouseCursor(4);
     }
@@ -41,6 +93,15 @@ void MainMenu::onExit() // called once, at the end of the scene
 {
     isSetUp = false;
     UnloadFont(font);
-    UnloadTexture(buttonTexture);
-    UnloadTexture(logoTexture);
+    UnloadTexture(simulatorButton_Texture);
+    UnloadTexture(logo_Texture);
+    UnloadTexture(graphsContainer_Texture);
+    UnloadTexture(graphsMenu_Texture);
+}
+
+float MainMenu::animateGraphsContainer()
+{
+    float animationCalc = GetFrameTime()* drag;
+    drag *= .9441F;
+    return animationCalc;
 }
